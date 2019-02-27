b178724d1419cf3feee4db3232e2835ff923de49
mohammed imtiyaz <mohammed.imtiyaz@signify.com>
2019-02-27 19:19:29 +0530

    Completed 2nd  module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
+
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
@@ -6,12 +8,13 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+
   render() {
     return (
       <div>
-        <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
       </div>
     );
   }
